96e84040459494b46885978db274ed38aea50d67
ossama970 <ossama999go@gmail.com>
2026-01-25 12:47:55 +0100

    Update spam.js

--- scripts/cmds/spam.js
+++ scripts/cmds/spam.js
@@ -1,24 +1,24 @@
 module.exports = {
   config: {
     name: "spam",
-    aurthor:"kim/zed",// Convert By Goatbot Zed
-     role: 2,
+    aliases: ["spam", "sp", "نك"],
+    author: "Hanji",// Convert By Goatbot Zed
+    role: 2,
     shortDescription: " ",
     longDescription: "",
     category: "sophia",
     guide: "{pn}"
   },
-
   onStart: async function ({ api, event, args }) {
-	const amount = parseInt(args[0]);
-	const message = args.slice(1).join(" ");
-
-	if (isNaN(amount) || !message) {
-		return api.sendMessage("Invalid usage. Usage: /spam [amount] [message]", event.threadID);
-	}
-
-	for (let i = 0; i < amount; i++) {
-		api.sendMessage(message, event.threadID);
-	}
+    const amount = parseInt(args[0]);
+    const message = args.slice(1).join(" ");
+    const delay = 3000; // تأخير 2 ثانية
+    if (isNaN(amount) || !message) {
+      return api.sendMessage("Invalid usage. Usage: /spam [amount] [message]", event.threadID);
+    }
+    for (let i = 0; i < amount; i++) {
+      api.sendMessage(message, event.threadID);
+      await new Promise(resolve => setTimeout(resolve, delay));
+    }
   },
 };
